1742af6efa53dc255ede6bc05c0df094a3563d1f
Brendan Moloney <moloney@ohsu.edu>
2019-10-28 21:52:21 -0700

    BF: Fix bug in local dir crawler

--- dcm/store/local_dir.py
+++ dcm/store/local_dir.py
@@ -54,16 +54,12 @@ def _dir_crawl_worker(res_q, root_path, recurse=True, file_ext='dcm',
     else:
         glob_comps.append('*')
     glob_exp = os.path.join(*glob_comps)
-    print('iglob arg: %s' % os.path.join(root_path, glob_exp))
-    for path in iglob(os.path.join(root_path, glob_exp), recursive=recurse):
-        print("found a path..")
+    for path in iglob(glob_exp, recursive=recurse):
         curr_files.append(path)
         if len(curr_files) == max_chunk:
-            print("Putting a chunk...")
             res_q.put(LocalDataChunk(curr_files))
             curr_files = []
     if len(curr_files) != 0:
-        print("Putting last chunk")
         res_q.put(LocalDataChunk(curr_files))
 
 
