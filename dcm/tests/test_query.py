ee811005f4656605a50779a825fad78ddd548661
Brendan Moloney <moloney@ohsu.edu>
2020-02-18 23:12:49 -0800

    TST: Improve QueryResult testing a bit
    Still lots of room for improvement, in particular with some of the
    behaviour when doing ops with different levels and subcounts.

--- dcm/tests/test_query.py
+++ dcm/tests/test_query.py
@@ -5,7 +5,9 @@ from pydicom.dataset import Dataset
 from ..query import QueryLevel, QueryResult, uid_elems, req_elems
 
 
-def make_dataset(attrs, level=QueryLevel.IMAGE):
+def make_dataset(attrs=None, level=QueryLevel.IMAGE):
+    if attrs is None:
+        attrs = {}
     ds = Dataset()
     for lvl in QueryLevel:
         for attr in req_elems[lvl]:
@@ -187,3 +189,81 @@ def test_json(hierarchy_data):
     json_dict = qr1.to_json_dict()
     qr2 = QueryResult.from_json_dict(json_dict)
     assert qr1 == qr2
+
+
+def test_intersect():
+    data = [make_dataset() for _ in range(5)]
+    qr1 = QueryResult(QueryLevel.IMAGE)
+    for ds in data[:3]:
+        qr1.add(ds)
+    qr2 = QueryResult(QueryLevel.IMAGE)
+    assert len(qr1 & qr2) == 0
+    for ds in data[3:]:
+        qr2.add(ds)
+    assert len(qr1 & qr2) == 0
+    qr2.add(data[2])
+    res = qr1 & qr2
+    assert len(res) == 1
+    assert data[2] in res
+
+
+def test_intersect_multi_level():
+    series_ds = make_dataset({'StudyInstanceUID': '1.2.3.4',
+                              'SeriesInstanceUID': '1.2.3.4.0',
+                              },
+                             level=QueryLevel.SERIES)
+    inst_ds = make_dataset({'StudyInstanceUID': '1.2.3.4',
+                            'SeriesInstanceUID': '1.2.3.4.0',
+                            'SOPInstanceUID': '1.2.3.4.0.0'
+                            },
+                           level=QueryLevel.IMAGE)
+    excl_ds = make_dataset({'StudyInstanceUID': '1.2.3.4',
+                            'SeriesInstanceUID': '1.2.3.4.1',
+                            'SOPInstanceUID': '1.2.3.4.1.0'
+                            },
+                           level=QueryLevel.IMAGE)
+    series_qr = QueryResult(QueryLevel.SERIES)
+    series_qr.add(series_ds)
+    inst_qr = QueryResult(QueryLevel.IMAGE)
+    inst_qr.add(inst_ds)
+    inst_qr.add(excl_ds)
+    assert len(inst_qr) == 2
+    res1 = series_qr & inst_qr
+    res2 = inst_qr & series_qr
+    assert res1 == res2
+    assert res1.level == res2.level == QueryLevel.IMAGE
+    assert len(res1) == 1
+    assert inst_ds in res1
+    assert excl_ds not in res1
+
+
+def test_union():
+    data = [make_dataset() for _ in range(5)]
+    qr1 = QueryResult(QueryLevel.IMAGE)
+    for ds in data[:3]:
+        qr1.add(ds)
+    qr2 = QueryResult(QueryLevel.IMAGE)
+    assert qr1 | qr2 == qr1
+    for ds in data[3:]:
+        qr2.add(ds)
+    res1 = qr1 | qr2
+    assert len(res1) == len(qr1) + len(qr2)
+    qr2.add(data[2])
+    res2 = qr1 | qr2
+    assert res1 == res2
+
+
+def test_subtract():
+    data = [make_dataset() for _ in range(5)]
+    qr1 = QueryResult(QueryLevel.IMAGE)
+    for ds in data[:3]:
+        qr1.add(ds)
+    qr2 = QueryResult(QueryLevel.IMAGE)
+    assert qr1 - qr2 == qr1
+    for ds in data[3:]:
+        qr2.add(ds)
+    assert qr1 - qr2 == qr1
+    qr2.add(data[2])
+    res = qr1 - qr2
+    assert len(res) == len(qr1) - 1
+    assert data[2] not in res
