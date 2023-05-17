1b89489698a5b4fbe3f7ca91d6b8a7f232a5034c
Brendan Moloney <moloney.brendan@gmail.com>
2023-05-17 13:47:23 -0700

    RF: Drop 3.7 as it is too much hassle to keep

--- dcm/util.py
+++ dcm/util.py
@@ -118,8 +118,7 @@ def is_primativish(t: Any) -> bool:
     origin = get_origin(t)
     if origin is Literal:
         return True
-    basetype = cattrs._compat.get_newtype_base(t)
-    if basetype is not None:
+    if (basetype := cattrs._compat.get_newtype_base(t)) is not None:
         return is_primativish(basetype)
     if origin in [Union, Tuple, List]:
         return all(is_primativish(ty) for ty in get_args(t))
